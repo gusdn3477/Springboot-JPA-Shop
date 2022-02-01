6b27635ad9897c04db78e52ffb79d5739248d754
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-02-01 10:33:25 +0900

    실전 예제 2 - 연관관계 매핑 시작

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,8 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -16,6 +15,9 @@ public class Member {
     private String city;
     private String secret;
     private String zipcode;
+
+    @OneToMany(mappedBy = "member") // 리드하는 변수 이름
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
